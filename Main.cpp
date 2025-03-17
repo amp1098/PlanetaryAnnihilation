233f9651c6b47a474ab4324643655a983d60bf3c
Chase <chase1098@gmail.com>
2025-03-17 10:46:18 -0400

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,64 @@
+#include "raylib.h"
+#include "string"
+#include <vector>
+
+//======== entry point =========//
+
+int main(void)
+{
+	const int sWidth = 2000;
+	const int sHeight = 1600;
+
+	// delete this later
+
+	const float posx{ 0.0f };
+	const float posy{ 0.0f };
+
+	// delete this later
+
+	InitWindow(sWidth, sHeight, "Planetary Annhilation");
+
+	// === RAYLIB STUFF ===
+
+	int spacing = 0;
+
+	Camera2D camera = { 0 };
+
+	camera.target = { 1.0f, 2.0f };
+
+	camera.target = { posx + 20.0f, posx + 20.0f };
+	camera.offset = { sWidth / 2.0f, sHeight / 2.0f };
+	camera.rotation = 0.0f;
+	camera.zoom = 1.0f;
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+
+		// camera target follows player
+		camera.target = { posx + 20, posx + 20 };
+
+		// zoom (and zoom limiter)
+		camera.zoom += ((float)GetMouseWheelMove() * 0.05f);
+		if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+		else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
+
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		BeginMode2D(camera);
+
+		EndMode2D();
+
+		EndDrawing();
+
+	}
+
+	CloseWindow();
+
+	return 0;
+
+}
+
---
+++ ent_row.h
@@ -0,0 +1,6 @@
+#ifndef ENT_ROW_H
+#define ENT_ROW_H
+
+
+
+#endif
