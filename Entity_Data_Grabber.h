a9e47dce009750aee7684dcc260cd23a80fb4344
Chase <chase1098@gmail.com>
2025-03-21 12:02:57 -0400

    also going to ditch this one, ugh coding is hard

---
+++ Entity_Data_Grabber.h
@@ -0,0 +1,22 @@
+#ifndef ENTITY_DATA_GRABBER_H
+#define ENTITY_DATA_GRABBER_H
+
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// this will read an entity data file (.xml) and assign each appropriate value
+// to each type of entity. This should happen only upon starting the game.
+
+int extract_entity_data(string file) {
+	ifstream ifs(file);
+
+	while()
+};
+
+
+
+
+#endif // !ENTITY_DATA_GRABBER_H
+
