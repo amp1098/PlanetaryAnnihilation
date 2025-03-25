6cf30472ab29ff86f2e2fed25dc1534d1c454b23
Chase <chase1098@gmail.com>
2025-03-25 12:58:49 -0400

    split useful_funcitons, code still compiles

--- useful_functions.h
+++ useful_functions.h
@@ -3,17 +3,8 @@
 
 #include <vector>
 #include <raylib.h>
+#include <raymath.h>
 
-Vector2 center_of_points(std::vector<Vector2> points) { // finds vector average of Vector2 array
-	Vector2 result{};
-
-	for (int i = 0; i < std::size(points); i++) { // summing vectors
-		result = result + points[i];
-	};
-
-	result = result / std::size(points); // scaling down according to size of array
-
-	return result;
-};
+Vector2 center_of_points(std::vector<Vector2> points);
 
 #endif
